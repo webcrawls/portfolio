34878a08f10186ce87436cc2701d519a658d8cc1
webcrawls <itskadenhere@gmail.com>
2026-02-08 00:19:09 -0500

    (🎨) some tweaks

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -16,14 +16,23 @@
 	<nav>
 		<ul>
 			<li>
-				<a href="#">Home</a>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/posts">Blogposts</a>
+			</li>
+			<li>
+				<a href="/">Projects</a>
 			</li>
 		</ul>
 	</nav>
 	<main>
 		{@render children()}
 	</main>
-	<footer></footer>
+	<footer>
+		<a href="#">Visit the Guestbook!</a>
+		<p><a href="#">(source)</a></p>
+	</footer>
 </div>
 
 <style>
@@ -33,13 +42,25 @@
 		box-sizing: border-box;
 	}
 
+	a {
+		color: white;
+	}
+
 	.layout {
 		width: 100vw;
 		height: 100vh;
 	}
 
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-	header, nav, footer {
+	header,
+	nav,
+	footer {
 		background-color: black;
 		color: white;
 	}
@@ -50,15 +71,20 @@
 		justify-content: center;
 		align-items: center;
 
-		border-bottom: 4px groove white
+		border-bottom: 4px groove white;
 	}
 
 	nav {
 		border-right: 4px groove white;
 
+		& ul {
+			display: flex;
+			flex-direction: column;
+			padding-left: 3rem;
+		}
+
 		& ul li {
-			padding-left: 5rem;
-			list-style: '(*) ';
+			list-style: "(*) ";
 		}
 	}
 
@@ -69,8 +95,9 @@
 	main {
 		background-color: gray;
 		color: black;
+		width: 100%;
+		height: 100%;
 	}
-
 
 	@media screen and (min-width: 0px) {
 		.layout {
@@ -86,15 +113,37 @@
 		nav { grid-area: 2 / 1 / 5 / 2; }
 		footer { grid-area: 5 / 1 / 6 / 3; }
 		main { grid-area: 2 / 2 / 5 / 3; } 
+
 	}
 
 	@media screen and (max-width: 500px) {
 		.layout {
 			display: grid;
 			grid-template-columns: 1fr;
-			grid-template-rows: 1fr min-content repeat(2, 1fr);
+			grid-template-rows: 150px min-content 1fr 100px;
 			grid-column-gap: 0px;
 			grid-row-gap: 0px;
+		}
+
+		header, nav, footer, main { grid-area: unset;}
+
+		header {
+			grid-row: 1;
+			grid-column: 1;
+		}
+		nav {
+			grid-row: 2;
+			grid-column: 1;
+			
+			height: 0px;
+		}
+		footer {
+			grid-row: 4;
+			grid-column: 1;
+		}
+		main {
+			grid-row: 3;
+			grid-column: 1;
 		}
 	}
 </style>
--- src/routes/(home)/posts/+page.svelte
+++ src/routes/(home)/posts/+page.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-    const posts = [{
-        title: 'testtesttesttest',
-        when: 'today',
-        by: 'kaden',
-        length: '100 words'
-    }]
+    const posts = [
+        {
+            title: "testtesttesttest",
+            when: "today",
+            by: "kaden",
+            length: "100 words",
+        },
+    ];
 </script>
 
 <ul>
-    {#each posts as {title, when, by, length}}
-    <li>
-        <p>{title}</p>
-        <p>{when} by <a href="#">{by}</a> | <a href="#">link</a> | {length}</p>
-    </li>
+    {#each posts as { title, when, by, length }}
+        <li>
+            <p><a href="#">{title}</a></p>
+            <p>
+                {when} by <a href="#">{by}</a> | <a href="#">link</a> | {length}
+            </p>
+        </li>
     {/each}
 </ul>
+
+<style>
+    ul {
+        display: flex;
+        flex-direction: column;
+        padding-left: 1.5rem;
+        list-style-type:decimal;
+    }
+
+    li a {
+        color: inherit;
+    }
+</style>
